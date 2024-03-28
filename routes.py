3d9c8fe5dcdb101c820e5e5926c5ec27c0c67dcc
Saifullah Khan <135647515+SaiFUllaH-KhaN1@users.noreply.github.com>
2024-03-28 21:31:35 +0500

    Update routes.py

--- routes.py
+++ routes.py
@@ -29,7 +29,7 @@ from langchain.chains import ConversationChain
 load_dotenv(dotenv_path="HUGGINGFACEHUB_API_TOKEN.env")
 # # Set the API key for OpenAI
 openai.api_key = os.getenv('OPENAI_API_KEY')
-client = OpenAI()
+
 import io
 import os
 
